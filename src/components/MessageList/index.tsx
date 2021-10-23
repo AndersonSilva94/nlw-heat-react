871a76813aa451b0f730efba094f1a50fc1bdda0
Anderson Silva <andssilva.23@gmail.com>
2021-10-23 17:29:54 -0400

    feat: :rocket: insere as informações e estiliza MessageList

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,5 +1,39 @@
+import styles from './styles.module.scss';
+import logoImage from '../../assets/logo.svg';
+
 export function MessageList() {
   return (
-    <h1>Message List</h1>
+    <div className={styles.messageListWrapper}>
+      <img src={logoImage} alt="Do/While 2021" />
+      <ul className={styles.messageList}>
+        <li className={styles.message}>
+          <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥</p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img src="https://github.com/AndersonSilva94.png" alt="Anderson Silva" />
+            </div>
+            <span>Andy Silva</span>
+          </div>
+        </li>
+        <li className={styles.message}>
+          <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥</p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img src="https://github.com/AndersonSilva94.png" alt="Anderson Silva" />
+            </div>
+            <span>Andy Silva</span>
+          </div>
+        </li>
+        <li className={styles.message}>
+          <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥</p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img src="https://github.com/AndersonSilva94.png" alt="Anderson Silva" />
+            </div>
+            <span>Andy Silva</span>
+          </div>
+        </li>
+      </ul>
+    </div>
   )
 }
