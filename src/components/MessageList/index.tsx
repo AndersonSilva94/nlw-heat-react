84bcc76d06ad017ca6eea6c35cc616b4c6936ceb
Anderson Silva <andssilva.23@gmail.com>
2021-10-24 00:17:01 -0400

    feat: :rocket: renderiza as mensagens em tempo real

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { api } from '../../services/api';
+import io from 'socket.io-client';
 import styles from './styles.module.scss';
 import logoImage from '../../assets/logo.svg';
 
@@ -12,6 +13,14 @@ type Message = {
   }
 }
 
+const messagesQueue: Message[] = [];
+
+const socket = io('http://localhost:4000');
+
+socket.on('new_message', newMessage => {
+  messagesQueue.push(newMessage);
+})
+
 export function MessageList() {
   const [messages, setMessages] = useState<Message[]>([])
 
@@ -19,6 +28,20 @@ export function MessageList() {
     api.get<Message[]>('messages/last3').then(response => {
       setMessages(response.data)
     })
+  }, [])
+
+  useEffect(() => {
+    setInterval(() => {
+      if (messagesQueue.length > 0) {
+        setMessages(prevState => [
+          messagesQueue[0],
+          prevState[0],
+          prevState[1]
+        ].filter(Boolean));
+
+        messagesQueue.shift()
+      }
+    }, 3000)
   }, [])
 
   return (
